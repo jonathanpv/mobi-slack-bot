2463519f4b681a70c20af03bc26cb2f0681260f2
Glitch (boulder-chevre) <none>
2019-10-16 00:22:30 +0000

    👁🍅 Checkpoint ./server.js:7616022/1771

--- server.js
+++ server.js
@@ -22,12 +22,7 @@ app.message('yeet', ({ message, say }) => {
   say(`YAW :yeet-dab:`);
 });
 
-app.message('F in the chat', ({ message, say }) => {
-  // say() sends a message to the channel where the event was triggered
-  say(`:pensive: :press-f:`);
-});
-
-app.message('rip', ({ message, say }) => {
+app.message(/^(rip|Rip|RiP|rIp|rIP|RIp|RIP).*/, ({ message, say }) => {
   // say() sends a message to the channel where the event was triggered
   say(`:pensive: :rip:`);
 });
@@ -37,6 +32,25 @@ app.message('boi', ({ message, say }) => {
   say(`:spongeboi:`);
 });
 
+app.message(/^(F in the chat|f in the chat).*/, async ({ context, say }) => {
+  // RegExp matches are inside of context.matches
+  // const greeting = context.matches[0];
+  say(`:pensive: :press-f:`);
+});
+
+// Listener middleware that filters out messages with 'bot_message' subtype
+function noBotMessages({ message, next }) {
+  if (!message.subtype || message.subtype !== 'bot_message') {
+     next();
+  }
+}
+
+// The listener only receives messages from humans
+app.message(noBotMessages, ({ message }) => console.log(
+  `(MSG) User: ${message.user}
+   Message: ${message.text}`
+  say(` `);
+));
 
 
 (async () => {
