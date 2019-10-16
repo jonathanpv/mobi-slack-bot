b94e8ec9894bc895eb3d7483b29e0f88c3c56545
Glitch (boulder-chevre) <none>
2019-10-16 02:51:34 +0000

    ✂️🌅 Checkpoint ./server.js:7446250/4

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const slack = new App({
 // Listens to incoming messages that contain "hello"
 slack.message("oof", ({ message, say }) => {
   // say() sends a message to the channel where the event was triggered
-  say(`big oof`);
+  say(`*big* _oof_`);
 });
 
 slack.message("yaw", ({ message, say }) => {
