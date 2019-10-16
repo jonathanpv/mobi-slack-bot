5680d26feccbc8f7eaaf8002da5a566e3c1c57eb
Glitch (hello-express) <none>
2019-10-16 00:02:15 +0000

    👯🐸 Checkpoint ./server.js:7616022/190

--- server.js
+++ server.js
@@ -37,10 +37,7 @@ app.message('boi', ({ message, say }) => {
   say(`:spongeboi:`);
 });
 
-app.message('goose coin', ({ message, say }) => {
-  // say() sends a message to the channel where the event was triggered
-  say(`https://slack-files.com/T96B9VBU0-FP3A32QCX-3405760330`);
-});
+
 
 (async () => {
   // Start your app
