4af6526d68cc594021276050f8fb84adf5d69561
Glitch (boulder-chevre) <none>
2020-05-18 17:28:21 +0000

    🎍🚌 Checkpoint ./src/channel-id.js:7616022/41

--- src/channel-id.js
+++ src/channel-id.js
@@ -8,6 +8,7 @@ module.exports = {
   general: "C96BA0316",
   socialCoding: "CG1QFD1J4",
   yeet: "CCNRV68BS",
-  hackathons: "CCUCG24KS"
+  hackathons: "CCUCG24KS",
+  random: "C96EA73S5"
   // add more channel ids here
 };
